ab29a5f212585cc1de686f6c953eee5d5d9151ab
Tommy <t@tommyhoang.com>
2019-01-28 13:58:46 -0800

    refactor form to use form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
     def new
+        @user = User.new(username: "pretender", email: "pre@tender.com")
     end
 
     def create
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        # @user = User.new(user_params)
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        # Used this for form_tag form
+
+        @user = User.new(user_params)
+        # Used this for regular form HTML, and form_for form
         if @user.save
             redirect_to new_user_path
         else
